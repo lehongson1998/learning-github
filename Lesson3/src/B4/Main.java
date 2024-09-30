50c0020ce7f4f273b88d65a37c6817fd0bc6fca5
sonle <lhongson2909@gmail.com>
2024-09-30 16:38:49 +0700

    update extends class training 2024/09/30

---
+++ Lesson3/src/B4/Main.java
@@ -0,0 +1,8 @@
+package B4;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal son = new Person("Mai jackson", 50, "187592899", 170.0f, 60.0f);
+        System.out.println(son);
+    }
+}
---
+++ Lesson3/src/B4/Person.java
@@ -0,0 +1,57 @@
+package B4;
+
+public class Person extends Animal{
+    private String idCard;
+    private float height;
+    private float weight;
+
+    public Person(){}
+
+    public Person(String idCard, float height, float weight) {
+        this.idCard = idCard;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public Person(String name, int age, String idCard, float height, float weight) {
+        super(name, age);
+        this.idCard = idCard;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name=" + this.getName() + "," +
+                "age=" + this.getAge() + "," +
+                "idCard='" + idCard + '\'' +
+                ", height=" + height +
+                ", weight=" + weight +
+                '}';
+    }
+}
