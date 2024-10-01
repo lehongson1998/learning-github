dad310d9f3b3f8b1f81ee84a708e15850d42515f
Le <sonhong.le2@dxc.com>
2024-10-01 11:18:20 +0700

    update new file2 and shell.sh of

--- shell.sh
+++ shell.sh
@@ -1,5 +1,6 @@
 echo $(date) >> file2.txt
 git add file2.txt shell.sh
 x=$(date)
-git commit -m "update new file2 and shell.sh `$(x)`"
+git commit -m "update new file2 and shell.sh of $(x)"
 git push
+git log --oneline
