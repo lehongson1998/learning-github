f5d1574d5216bd3b26190ec90d08e6253821f500
Le <sonhong.le2@dxc.com>
2024-10-01 11:19:31 +0700

    update new file2 and shell.sh of Tue Oct  1 11:19:31 SEAST 2024

--- shell.sh
+++ shell.sh
@@ -1,6 +1,6 @@
 echo $(date) >> file2.txt
 git add file2.txt shell.sh
 x=$(date)
-git commit -m "update new file2 and shell.sh of $(x)"
+git commit -m "update new file2 and shell.sh of $x"
 git push
-git log --oneline
+git log --oneline && q
