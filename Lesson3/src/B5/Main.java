f83fca9ac157f9cd02a315ee7b730b8de3ce4558
sonle <lhongson2909@gmail.com>
2024-10-01 21:41:07 +0700

    training equals method overide ver 2.0.0

--- Lesson3/src/B5/Main.java
+++ Lesson3/src/B5/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Animal tom = new Animal("Toms", 3, 20.0f);
         Animal jerry = new Animal("Tom", 3, 20.0f);
-
+        //print result of equals method
         System.out.println(tom.equals(jerry));
     }
 }
