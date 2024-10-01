1c6b6644b40573d4032e703b4f9a955a244fb7a2
Le <sonhong.le2@dxc.com>
2024-10-01 10:41:46 +0700

    add new comment Ãin class Main.java

--- Lesson3/src/B4/Main.java
+++ Lesson3/src/B4/Main.java
@@ -2,6 +2,7 @@ package B4;
 
 public class Main {
     public static void main(String[] args) {
+        // added new comments
         Animal son = new Person("Mai jackson", 50, "187592899", 170.0f, 60.0f);
         System.out.println(son);
         son.name = son.name.toUpperCase();
