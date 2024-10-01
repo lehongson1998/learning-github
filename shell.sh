122f638374c921bd6367172a55ed0baddc413951
Le <sonhong.le2@dxc.com>
2024-10-01 13:52:17 +0700

    update git_log_time and shell.sh of Tue Oct  1 13:52:17 SEAST 2024

--- shell.sh
+++ shell.sh
@@ -1,5 +1,5 @@
 echo $(date) >> git_log_time.log
-git add git_log_time.log shell.sh
+git add git_log_time.log shell.sh $1 $2
 x=$(date)
 git commit -m "update git_log_time and shell.sh of $x"
 git push
