31a18af301cc8b6b063cbfb245eab568df36a5d4
sonle <lhongson2909@gmail.com>
2024-10-03 08:59:23 +0700

    up B6 on github

---
+++ Lesson3/src/B6/Father.java
@@ -0,0 +1,40 @@
+package B6;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Father {
+    private final int A = 123;
+    private final String PATH = "C:\\SMBC\\learning-github\\git_log_time.log";
+
+    public Father() {
+    }
+
+    public int getA() {
+        return A;
+    }
+
+    public String getPath() {
+        return PATH;
+    }
+
+    @Override
+    public String toString() {
+        return "Father{" +
+                "A=" + A +
+                ", PATH='" + PATH + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Father f = new Father();
+        File file = new File(f.PATH);
+        System.out.println(f);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+        }
+    }
+}
