2b3bda80c7a8a69a9df3fb10136307420c26067f
sonle <lhongson2909@gmail.com>
2024-10-05 17:01:03 +0700

    update package LearnStringClass on github

---
+++ Lesson3/src/LearnStringClass/Main.java
@@ -0,0 +1,32 @@
+package LearnStringClass;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("enter String:");
+            String str = scanner.nextLine();
+            System.out.println("str size = " + str.length());
+            System.out.println("str[3] = " + str.charAt(3));
+            char[] chars = new char[100];
+            str.getChars(0, str.length(), chars, 0);
+            for (char aChar : chars) {
+                if (aChar == 0) break;
+                System.out.print(aChar + " ");
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+
+        String s1 = "Le Hong Son";
+        String s2 = "le hong son";
+        String s3 = "Le Hong Son";
+
+        System.out.println("\ns1 equal s2 : " + s1.equals(s2));
+        System.out.println("s1 equal s3 : " + s1.equals(s3));
+        System.out.println("s1 equalIgnoreCase s2 " + s1.equalsIgnoreCase(s2));
+        System.out.println("s1 equalIgnoreCase s3 " + s1.equalsIgnoreCase(s3));
+    }
+}
