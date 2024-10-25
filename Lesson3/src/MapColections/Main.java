502614c0ea44ffd31d3a43eb54ec057db777cc4c
sonle <lhongson2909@gmail.com>
2024-10-25 11:26:55 +0700

    learning map colection

---
+++ Lesson3/src/MapColections/Main.java
@@ -0,0 +1,20 @@
+package MapColections;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "A");
+        map.put(2, "B");
+        map.put(3, "C");
+        map.put(4, "D");
+        map.put(5, "E");
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+            if (entry.getKey() == 4) break;
+        }
+    }
+}
